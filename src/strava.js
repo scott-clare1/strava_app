7a29c42d7b9f42ed68508dd6acde45cf3d665e32
Scott Clare <scott_clare1@yahoo.co.uk>
2023-11-27 17:31:20 +0000

    adding code to select laps and get average time per mile

--- src/strava.js
+++ src/strava.js
@@ -7,7 +7,51 @@ function fetchData(link){
     })
 }
 
-function createAccordionItem(title, content) {
+function createDropdown(jsonArray, id) {
+  const dropdown = document.createElement('select');
+  dropdown.multiple = true;
+  dropdown.id = id + "_dropdown";
+  for (const lap of jsonArray) {
+    const option = document.createElement('option');
+    option.value = lap.name;
+    option.text = lap.name; // Example text based on the value
+    dropdown.appendChild(option);
+}
+dropdown.addEventListener('change', function () {
+    // Get the selected values
+    const selectedValues = Array.from(this.selectedOptions, option => option.value);
+    let sumTime = 0
+    let sumDistance = 0
+    jsonArray.forEach(item => {
+      if (selectedValues.includes(item.name)) {
+        sumTime += item.elapsed_time;
+        sumDistance += item.distance;
+      }
+    })
+    var outputDiv = document.getElementById(id + "_result")
+    outputDiv.innerHTML = 26.8224 / (sumDistance / sumTime);
+});
+return dropdown;
+}
+
+function createDivs(jsonArray, contentDiv, id) {
+  // Loop through the array
+  jsonArray.forEach(item => {
+    // Create a div element
+    const div = document.createElement('div');
+
+    // Set content or attributes based on JSON properties
+    div.innerHTML = `${item.name}: <br> Distance: ${item.distance} <br> Time: ${item.elapsed_time}`;
+
+    // Append the div to the container
+    contentDiv.appendChild(div);
+  });
+  contentDiv.appendChild(createDropdown(jsonArray, id))
+  return contentDiv;
+}
+
+
+function createAccordionItem(title, contentArray, id) {
     const item = document.createElement('div');
     item.classList.add('accordion-item');
 
@@ -18,10 +62,13 @@ function createAccordionItem(title, content) {
 
     const contentDiv = document.createElement('div');
     contentDiv.classList.add('accordion-item-content');
-    contentDiv.textContent = content;
 
     item.appendChild(header);
-    item.appendChild(contentDiv);
+    const result = document.createElement("div")
+    let result_id = id + "_result";
+    result.id = result_id;
+    item.appendChild(createDivs(contentArray, contentDiv, id));
+    item.appendChild(result)
 
     return item;
   }
@@ -34,23 +81,25 @@ function createAccordionItem(title, content) {
   // Function to initialize the accordion
   function initializeAccordion(containerId, data) {
     const accordionContainer = document.getElementById(containerId);
-    console.log(data)
+
     data.forEach(itemData => {
       var title = itemData.name + " - " + itemData.start_date;
-      var type = itemData.sport_type;
-      if (type = "Run"){
-        const accordionItem = createAccordionItem(title, itemData.distance);
-        accordionContainer.appendChild(accordionItem);
-      }
+      var laps = itemData.laps;
+      let id = itemData.id.toString();
+      const accordionItem = createAccordionItem(title, laps, id);
+      accordionContainer.appendChild(accordionItem);
     });
   }
 
   function processArray(res, arrayData) {
     var segments = [];
     arrayData.forEach(item => {
-      const segments_link = `https://www.strava.com/api/v3/activities/${item.id}?access_token=${res.access_token}`
-      var segment = fetchData(segments_link));
-      segments.push(segment);
+      var type = item.sport_type;
+      if (type == "Run"){
+        const segments_link = `https://www.strava.com/api/v3/activities/${item.id}?access_token=${res.access_token}`
+        var segment = fetchData(segments_link);
+        segments.push(segment);
+      }
     });
     return segments;
   }
@@ -61,7 +110,6 @@ function createAccordionItem(title, content) {
       .then(activities => {
         Promise.all(processArray(res, activities))
         .then((results) => {
-          console.log(results);
           initializeAccordion('accordionContainer', results);
         })
       })
